5981a1660c745e2574e828dc992f36fcb7ff0ef0
jeanmfdias <jeanmfdias@gmail.com>
2021-09-02 21:36:49 -0300

    added the bootstrap with laravel ui

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,14 +1,27 @@
 <!doctype html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>Series Guide</title>
-    </head>
-    <body>
-        <ul>
-            @foreach($series as $serie)
-                <li>{{ $serie }}</li>
-            @endforeach
-        </ul>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>Series Guide</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<br>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Series</h1>
+    </div>
+    <div class="form-group">
+        <a href="" class="btn btn-primary">Adicionar</a>
+    </div>
+    <ul class="list-group">
+        @foreach($series as $serie)
+            <li class="list-group-item">{{ $serie }}</li>
+        @endforeach
+    </ul>
+</div>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 
 
 Route::get('/series', [SeriesController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
